c220f45939300579ec1bf52c67a937990f1c12ce
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-11-21 09:46:28 +0100

    dock title out screen fix

--- utils/req.py
+++ utils/req.py
@@ -2,12 +2,13 @@ import requests
 
 
 def req(method, url, data=None, headers=None):
-    if method == "put":
-        return requests.put(url, json=data, headers=headers)
-    elif method == "post":
-        return requests.post(url, json=data, headers=headers)
-    elif method == "get":
-        return requests.get(url)
-    else:
+    try:
+        if method == "put":
+            return requests.put(url, json=data, headers=headers)
+        elif method == "post":
+            return requests.post(url, json=data, headers=headers)
+        elif method == "get":
+            return requests.get(url)
+    except:
         print("Error : Wrong method input")
 

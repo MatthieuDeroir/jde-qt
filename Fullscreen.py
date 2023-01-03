0770baeb8890119567599b9b9afff6bcd5ed9af3
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-01-03 14:48:04 +0100

    e

--- Fullscreen.py
+++ Fullscreen.py
@@ -71,8 +71,8 @@ class Ui_Fullscreen(object):
 
                 self.display_label = DisplayLabel(self.centralwidget, self.index)
                 self.display_label.setObjectName("display_label")
-                self.display_label.setStyleSheet("background-image: url(./fullscreen.png)")
-                self.display_label.setStyleSheet("background-image:url(./fullscreenBlack.png)")
+                self.display_label.setStyleSheet("background-image: url(./medias/fullscreen.png)")
+                self.display_label.setStyleSheet("background-image:url(./medias/fullscreenBlack.png)")
                 self.display_label.setGeometry(QtCore.QRect(0, 0, screen_width, screen_height))
 
             self.retranslateUi(MainWindow)

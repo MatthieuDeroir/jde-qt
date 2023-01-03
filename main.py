362e195d78515c662755b42ef4ec2a654f0e74a6
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-01-03 13:46:27 +0100

    path c

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from datetime import datetime
 import subprocess
 import requests
 import time
+
+
 #
 # class WinForm(QWidget):
 #     def __init__(self,parent=None):
@@ -76,7 +78,7 @@ class Main(QtWidgets.QMainWindow):
         # self.ui.setupUi(self)
         self.timer = QTimer(self)
         self.timer.start(1000)
-        #self.timer.start(media . duration)
+        # self.timer.start(media . duration)
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.move(0, 0)
         self.timer.timeout.connect(self.updateMode)
@@ -84,44 +86,42 @@ class Main(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.screenBlanking)
 
     def getOption(self):
-        #TODO : Implicitement, la selection d'un mode signifie également la séléction d'un format de média (video ou image)
-            if self.current_mode is not self.mode:
-                self.hasChangedDisplayMode = True
-                self.current_mode = self.mode
+        # TODO : Implicitement, la selection d'un mode signifie également la séléction d'un format de média (video ou image)
+        if self.current_mode is not self.mode:
+            self.hasChangedDisplayMode = True
+            self.current_mode = self.mode
 
-            if self.mode == 3:
-                self.timer.start(self.medias[self.index]['duration'] * 1000)
-                if self.index > 4:
-                    if len(self.medias) != 5:
-                        self.ui = Ui_Fullscreen(self.index)
-                        self.ui.setupUi(self)
-                        self.lastMedia = True
-                    if self.index >= len(self.medias) - 1:
-                        self.index = 3
-                elif self.index == 4 and self.lastMedia == True:
-                    self.ui = Ui_Truckscreen()
+        if self.mode == 3:
+            if self.index > 4:
+                if len(self.medias) != 5:
+                    self.timer.start(self.medias[self.index]['duration'] * 1000)
+                    self.ui = Ui_Fullscreen(self.index)
                     self.ui.setupUi(self)
-                    self.lastMedia = False
-                self.index = self.index + 1
-
-            elif self.mode == 2 and self.hasChangedDisplayMode == True:
-                self.ui = Ui_Fullscreen(0)
-                
-            elif self.mode == 1 and self.hasChangedDisplayMode == True:
-                self.ui = Ui_Splitscreen()
-                
-            elif self.mode == 0 and self.hasChangedDisplayMode == True:
-                self.ui = Ui_Shutdown(-1)
-
-
-                
-            if self.hasChangedDisplayMode and self.mode != 3:
-                self.timer.start(1000)
+                    self.noMedia = True
+                if self.index >= len(self.medias) - 1:
+                    self.index = 3
+            elif self.index == 4 and self.noMedia == True:
+                self.timer.start(self.medias[self.index]['duration'] * 1000)
+                self.ui = Ui_Truckscreen()
                 self.ui.setupUi(self)
-                self.hasChangedDisplayMode = False
-                self.lastMedia = True
-        
-            return self.index
+                self.noMedia = False
+
+        elif self.mode == 2 and self.hasChangedDisplayMode == True:
+            self.ui = Ui_Fullscreen(0)
+
+        elif self.mode == 1 and self.hasChangedDisplayMode == True:
+            self.ui = Ui_Splitscreen()
+
+        elif self.mode == 0 and self.hasChangedDisplayMode == True:
+            self.ui = Ui_Shutdown(-1)
+
+        if self.hasChangedDisplayMode and self.mode != 3:
+            self.timer.start(1000)
+            self.ui.setupUi(self)
+            self.hasChangedDisplayMode = False
+            self.lastMedia = True
+
+        return self.index
 
     def updateMode(self):
         try:
@@ -142,9 +142,9 @@ class Main(QtWidgets.QMainWindow):
         self.current_hour = now.strftime("%H")
         self.current_minute = now.strftime("%M")
         self.current_days = now.strftime("%A")
-        #print("Il est ", self.current_hour, ":", self.current_minute,  self.current_days)
+        # print("Il est ", self.current_hour, ":", self.current_minute,  self.current_days)
         try:
-            #print("La veille est prévue entre ", self.start[0], ":", self.start[1], " et ", self.stop[0], ":", self.stop[1])
+            # print("La veille est prévue entre ", self.start[0], ":", self.start[1], " et ", self.stop[0], ":", self.stop[1])
             if self.current_days == "Sunday":
                 if self.dstart[0] < self.current_hour:
                     self.veille = False
@@ -198,13 +198,13 @@ class Main(QtWidgets.QMainWindow):
                 if self.start[0] <= self.stop[0]:
                     if self.start[0] < self.current_hour:
                         if self.stop[0] > self.current_hour:
-                            self.display("on")   
+                            self.display("on")
                         elif self.stop[0] == self.current_hour and self.stop[1] > self.current_minute:
                             self.display("on")
 
                         else:
                             self.display("off")
-                            
+
 
                     elif self.start[0] == self.current_hour and self.start[1] <= self.current_minute:
                         if self.stop[0] > self.current_hour:
@@ -214,7 +214,7 @@ class Main(QtWidgets.QMainWindow):
                             self.display("on")
                         else:
                             self.display("off")
-                            
+
 
                 elif self.start[0] > self.stop[0]:
                     if self.start[0] < self.current_hour:
@@ -223,18 +223,18 @@ class Main(QtWidgets.QMainWindow):
 
                         elif self.stop[0] == self.current_hour and self.stop[1] > self.current_minute:
                             self.display("off")
-                            
+
 
                         else:
                             self.display("off")
-                           
+
                     elif self.start[0] == self.current_hour and self.start[1] <= self.current_minute:
                         if self.stop[0] > self.current_hour:
                             self.display("off")
-                           
+
                         elif self.stop[0] == self.current_hour and self.stop[1] > self.current_minute:
                             self.display("off")
-                            
+
                         else:
                             self.display("off")
 
@@ -243,21 +243,17 @@ class Main(QtWidgets.QMainWindow):
 
     def display(self, state):
         # print("PROCESS", state)
-        #subprocess.Popen(["xset", "-d", ":0", "dpms", "force",
+        # subprocess.Popen(["xset", "-d", ":0", "dpms", "force",
         #              state], stdout=subprocess.PIPE)
-        
 
         if state == "on" and self.veille == False:
-          
-            requests.put( ip_mode_put, data={'activeMode': self.modeBack})
+
+            requests.put(ip_mode_put, data={'activeMode': self.modeBack})
             self.veille = True
         elif state == "off" and self.veille == True:
-            
-            requests.put( ip_mode_put, data={'activeMode': '0'}) 
+
+            requests.put(ip_mode_put, data={'activeMode': '0'})
             self.veille = False
-            
-
-
 
 
 if __name__ == '__main__':

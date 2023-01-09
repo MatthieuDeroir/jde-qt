64dc4785628aa01c4e096436afa7895152ece825
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-01-09 14:37:41 +0100

    Splitscreen video

--- Fullscreen.py
+++ Fullscreen.py
@@ -26,51 +26,58 @@ class Ui_Fullscreen(object):
     def __init__(self, index):
         self.index = index
         self.path = "./"
+        self.format = ""
         self.current_path = './'
         timer = QTimer()
         timer.start(1000)  # Update the UI every 1 second
-        timer.timeout.connect(self.updateUi)
+        # timer.timeout.connect(self.updateUi)
         timer.timeout.connect(self.setupUi)
         self.fetched_datas = ""
+        self.fetched_datas = req("get", ip_fs).json()
 
-    def updateUi(self):
-        try:
-            self.fetched_datas = req("get", ip_fs).json()
-            self.path = self.fetched_datas[self.index]['path']
-        except:
-            print("cant fetch datas")
+    # def updateUi(self):
+    #     try:
+    #
+    #     except:
+    #         print("cant fetch datas")
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(192, 433)
-        if self.fetched_datas[self.index]['format'] == 'mp4':
-            print(self.path)
-            videoWidget = QVideoWidget()
-            wid = QtWidgets.QWidget(MainWindow)
-            MainWindow.setCentralWidget(wid)
-            self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-            layout = QVBoxLayout()
-            layout.addWidget(videoWidget)
-            wid.setLayout(layout)
-            self.mediaPlayer.setObjectName("display_label")
-            self.mediaPlayer.setVideoOutput(videoWidget)
-            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile('/home/pi/jde/panel/GUI/Chats.mp4')))
-            self.mediaPlayer.play()
-        else:
-            self.centralwidget = QtWidgets.QWidget(MainWindow)
-            self.centralwidget.setObjectName("centralwidget")
-            self.label = QtWidgets.QLabel(self.centralwidget)
-            self.label.setGeometry(QtCore.QRect(0, 0, screen_width, screen_height))
-            self.label.setObjectName("label")
-            MainWindow.setCentralWidget(self.centralwidget)
+        try:
+            self.fetched_datas = req("get", ip_fs).json()
+            self.format = self.fetched_datas[self.index]['path']
+            print(self.fetched_datas)
 
-            self.display_label = DisplayLabel(self.centralwidget, self.index)
-            self.display_label.setObjectName("display_label")
-            # self.display_label.setStyleSheet("background-image: url(medias/fullscreen.png)")
-            # self.display_label.setStyleSheet("background-image:url(medias/fullscreenBlack.png)")
+            if self.fetched_datas[self.index]['format'] == 'mp4':
+                videoWidget = QVideoWidget()
+                wid = QtWidgets.QWidget(MainWindow)
+                MainWindow.setCentralWidget(wid)
+                self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+                layout = QVBoxLayout()
+                layout.addWidget(videoWidget)
+                wid.setLayout(layout)
+                self.mediaPlayer.setObjectName("display_label")
+                self.mediaPlayer.setVideoOutput(videoWidget)
+                self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile('/home/pi/jde/panel/GUI/Chats.mp4')))
+                self.mediaPlayer.play()
+            else:
+                self.centralwidget = QtWidgets.QWidget(MainWindow)
+                self.centralwidget.setObjectName("centralwidget")
+                self.label = QtWidgets.QLabel(self.centralwidget)
+                self.label.setGeometry(QtCore.QRect(0, 0, screen_width, screen_height))
+                self.label.setObjectName("label")
+                MainWindow.setCentralWidget(self.centralwidget)
 
-            self.display_label.setGeometry(QtCore.QRect(0, 0, screen_width, screen_height))
-            self.display_label.setScaledContents(True)
+                self.display_label = DisplayLabel(self.centralwidget, self.index)
+                self.display_label.setObjectName("display_label")
+                # self.display_label.setStyleSheet("background-image: url(medias/fullscreen.png)")
+                # self.display_label.setStyleSheet("background-image:url(medias/fullscreenBlack.png)")
+
+                self.display_label.setGeometry(QtCore.QRect(0, 0, screen_width, screen_height))
+                self.display_label.setScaledContents(True)
+        except:
+            print("cant fetch datas")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
